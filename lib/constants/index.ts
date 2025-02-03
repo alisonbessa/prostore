83a6b478834e42b9f1efdf88b6556f6f450955c4
alisonbessa <alisonbessa@gmail.com>
2025-02-03 15:33:09 -0300

    feat: enhance metadata configuration with dynamic title and server URL

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -2,3 +2,5 @@ export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Next Store";
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   "Next Store is a simple e-commerce application built with Next.js and Stripe.";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
