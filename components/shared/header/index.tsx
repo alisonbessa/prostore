15dabf1dbe89f6b36d5d2543ad78ae11acbbcf75
alisonbessa <alisonbessa@gmail.com>
2025-02-03 16:13:39 -0300

    feat: add theme toggle functionality with dropdown menu

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,6 +3,7 @@ import { ShoppingCart, UserIcon } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import ModeToggle from "./mode-toggle";
 
 const Header = () => {
   return (
@@ -23,12 +24,13 @@ const Header = () => {
           </Link>
         </div>
         <div className="space-x-2">
+          <ModeToggle />
           <Button asChild variant="ghost">
             <Link href="/cart">
               <ShoppingCart size={24} /> Cart
             </Link>
           </Button>
-          <Button asChild variant="ghost">
+          <Button asChild>
             <Link href="/sign-in">
               <UserIcon size={24} /> Sign In
             </Link>
