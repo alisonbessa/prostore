a07aaafe775aa9cbdb185aec7a3b9237a577aefd
alisonbessa <alisonbessa@gmail.com>
2025-02-03 15:11:16 -0300

    feat: implement initial layout and components for Next Store application

---
+++ app/(root)/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+
+const Homepage = () => {
+  return <Button>Click me</Button>;
+};
+
+export default Homepage;
---
+++ lib/constants/index.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Next Store";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "Next Store is a simple e-commerce application built with Next.js and Stripe.";
