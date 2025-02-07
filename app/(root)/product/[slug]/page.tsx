56c4c690f639cb65a00cbcc1e2fbfcbc15ae7043
alisonbessa <alisonbessa@gmail.com>
2025-02-07 15:12:42 -0300

    feat: add product images component and integrate it into product details page

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import ProductImages from "@/components/shared/product/product-images";
 import ProductPrice from "@/components/shared/product/product-price";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
@@ -16,35 +17,38 @@ const ProductDetailsPage = async (props: {
   return (
     <>
       <section>
-        <div className="grid grid-cols-1 md:grid-cols-5"></div>
-        {/* Images Column */}
-        <div className="col-span-2">{/* Images Component */}</div>
-        {/* Product Details Column */}
-        <div className="col-span-2 p5">
-          <div className="flex flex-col gap-6">
-            <p>
-              {product.brand} - {product.category}
-            </p>
-            <h1 className="h3-bold">{product.name}</h1>
-            <p>
-              {product.rating} of {product.numReviews} reviews
-            </p>
-            <div className="flex flex-col sm:flex-row sm:items-center gap-3">
-              <ProductPrice
-                value={Number(product.price)}
-                className="w-24 rounded-full - bg-green-100 text-green-700 px-5 py-2"
-              />
-            </div>
+        <div className="grid grid-cols-1 md:grid-cols-5">
+          {/* Images Column */}
+          <div className="col-span-2">
+            <ProductImages images={product.images} />
           </div>
-          <div className="my-10">
-            <p className="font-semibold">Description</p>
-            <p>{product.description}</p>
+          {/* Product Details Column */}
+          <div className="col-span-2 p5">
+            <div className="flex flex-col gap-6">
+              <p>
+                {product.brand} - {product.category}
+              </p>
+              <h1 className="h3-bold">{product.name}</h1>
+              <p>
+                {product.rating} of {product.numReviews} reviews
+              </p>
+              <div className="flex flex-col sm:flex-row sm:items-center gap-3">
+                <ProductPrice
+                  value={Number(product.price)}
+                  className="w-24 rounded-full - bg-green-100 text-green-700 px-5 py-2"
+                />
+              </div>
+            </div>
+            <div className="my-10">
+              <p className="font-semibold">Description</p>
+              <p>{product.description}</p>
+            </div>
           </div>
 
           {/* Action Column */}
           <div>
             <Card>
-              <CardContent>
+              <CardContent className="pt-5">
                 <div className="mb-2 flex justify-between">
                   <div>Price</div>
                   <div>
@@ -54,9 +58,13 @@ const ProductDetailsPage = async (props: {
                 <div className="mb-2 flex justify-between">
                   <div>Status</div>
                   {product.stock > 0 ? (
-                    <Badge variant="outline">In Stock</Badge>
+                    <Badge variant="outline" className="rounded-full">
+                      In Stock
+                    </Badge>
                   ) : (
-                    <Badge variant="destructive">Out of Stock</Badge>
+                    <Badge variant="destructive" className="rounded-full">
+                      Out of Stock
+                    </Badge>
                   )}
                 </div>
                 {product.stock > 0 && (
